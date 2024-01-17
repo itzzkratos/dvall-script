3964091de68c3644b3fdbdc93e1506a1828bc426
itzzkratos <147372898+itzzkratos@users.noreply.github.com>
2024-01-16 19:45:18 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+webhookURL = "https://discord.com/api/webhooks/1196974592464527410/vohmP_mPAggF94HVszFNNnaSTqwe_zB6teFuvl7QQBz6_czSHaxkZKHvB-r3mZOn6a1-"
+
+Config = {
+    commandName = "dvall",
+
+    restricCommand = true
+} 
